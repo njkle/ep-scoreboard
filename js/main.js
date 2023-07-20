c81f25f86667360462035a268f40c7e279a5ddb6
Adam <137445942+njkle@users.noreply.github.com>
2023-07-20 17:29:51 +0200

    Update for Github Pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-fetch('../data/individual_grades0.json')
+fetch("./data/individual_grades0.json")
 .then(response => response.json())
 .then(data => {
     data.forEach(mep => generateCard(mep, ".gallery"));
 })
-.catch(error => console.log('Error:', error));
+.catch(error => console.log("Error:", error));
