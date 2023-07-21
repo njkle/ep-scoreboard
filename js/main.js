0907b03987a05e4949afc66d87087c361a897813
Adam <137445942+njkle@users.noreply.github.com>
2023-07-21 13:36:19 +0200

    Add search bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
+let data;
+
+//Fetch data and build gallery
 fetch("data/individual_grades0.json")
 .then(response => response.json())
-.then(data => {
+.then(responseData => {
+    data = responseData;
     data.forEach(mep => generateCard(mep, ".gallery"));
 })
 .catch(error => console.log("Error:", error));
+
+//Search bar
+const searchInput = document.querySelector('.input');
+searchInput.addEventListener(
+    'input', (e) => {
+        let input = e.target.value;
+        if(input && input.trim().length > 0){
+            input = input.trim().toLowerCase();
+            const filteredMeps = data.filter(
+                mep => {
+                    return mep.full_name.toLowerCase().includes(input)
+                }
+            );
+
+            document.querySelector(".gallery").innerHTML = "";
+            filteredMeps.forEach(mep => generateCard(mep, ".gallery"));
+        }
+        else {
+            document.querySelector(".gallery").innerHTML = "";  // Clear gallery
+            data.forEach(mep => generateCard(mep, ".gallery"));  // Regenerate gallery with all MEPs
+        }
+    }
+);
